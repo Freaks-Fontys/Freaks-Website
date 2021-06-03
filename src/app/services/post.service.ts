10b3ba40e00a699683bd2711b9ece17fd5089262
Twan Beeren <beerentwan2709@gmail.com>
2021-06-03 13:39:05 +0200

    Added environment vars

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,38 +2,38 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Post } from '../models/post';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-  baseURL = 'https://localhost:44360/api/Post';
 
   constructor(
     private http: HttpClient
   ) { }
 
   get(id: number): Promise<Post>{
-    return this.http.get<Post>(this.baseURL + '/' + id).toPromise();
+    return this.http.get<Post>(`${environment.apiUrl}/api/Post` + id).toPromise();
   }
 
   getMultiple(paginationStart: number, paginationEnd: number): Promise<Post[]>{
     const params = new HttpParams();
     params.set('start', paginationStart.toString());
     params.set('end', paginationEnd.toString());
-    return this.http.get<Post[]>(this.baseURL, {params}).toPromise();
+    return this.http.get<Post[]>(`${environment.apiUrl}/api/Post`, {params}).toPromise();
   }
 
   create(post: Post): Promise<Post>{
-    return this.http.post<Post>(this.baseURL, post).toPromise();
+    return this.http.post<Post>(`${environment.apiUrl}/api/Post`, post).toPromise();
   }
 
   update(post: Post): Promise<Post>{
-    return this.http.put<Post>(this.baseURL, post).toPromise();
+    return this.http.put<Post>(`${environment.apiUrl}/api/Post`, post).toPromise();
   }
 
   // delete(post: Post): Promise<Post>{
-  //   return this.http.delete(this.baseURL, post.id).toPromise();
+  //   return this.http.delete(`${environment.apiGatewayHost}/api/Post`, post.id).toPromise();
   // }
 
 }
