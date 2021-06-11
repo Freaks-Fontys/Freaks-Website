96a71f34fe461fa4e915d0cd7c705b79a16021b3
Twan Beeren <beerentwan2709@gmail.com>
2021-06-11 11:34:14 +0200

    Readded authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent  },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard]  },
   { path: 'login',  component: LoginComponent },
   { path: '',   redirectTo: '/home', pathMatch: 'full' }
 ];
