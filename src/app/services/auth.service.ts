24a0e960b2905b4143fc4f10e3294726843af6a0
Twan Beeren <beerentwan2709@gmail.com>
2021-06-15 14:56:00 +0200

    Updated production env

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { UserLoginMsg } from '../models/messages/user-login-msg';
 import { UserRegisterMsg } from '../models/messages/user-register-msg';
 import { User } from '../models/user';
